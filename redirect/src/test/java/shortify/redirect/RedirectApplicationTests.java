f4a9a9547d83f0e73c7f7bdaf503d9a0cee082cb
Pedro Guimarães <pedro.guimaraes@pagbrasil.com>
2024-11-20 21:00:31 -0300

    feat: create redirect service

---
+++ redirect/src/test/java/shortify/redirect/RedirectApplicationTests.java
@@ -0,0 +1,13 @@
+package shortify.redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
